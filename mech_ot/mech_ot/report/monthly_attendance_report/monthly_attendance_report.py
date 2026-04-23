aef6425366f006f7294a707b7df8fe29c41f7ec6
jigar-greycube <jigar@greycube.in>
2026-04-23 13:48:29 +0530

    feat:add count of half day absent

--- mech_ot/mech_ot/report/monthly_attendance_report/monthly_attendance_report.py
+++ mech_ot/mech_ot/report/monthly_attendance_report/monthly_attendance_report.py
@@ -806,6 +806,7 @@ def get_data(filters):
 			elif d['status'] == "HD":
 				sts_row['total'] = sts_row['total'] + 0.5
 				sts_row['total_days_present'] = sts_row['total_days_present'] + 0.5
+				sts_row['total_days_absent'] = sts_row['total_days_absent'] + 0.5
 			elif d['status'] in ["A", "L"]:
 				sts_row['total_days_absent'] = sts_row['total_days_absent'] + 1
 			elif d['status'] == "HP":
